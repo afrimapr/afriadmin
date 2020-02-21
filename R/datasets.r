6679c9a69252f9b3231fa03fe2588b6eb96a8a24
AndySouth <southandy@gmail.com>
2020-02-21 10:24:02 +0000

    document data

---
+++ R/datasets.r
@@ -0,0 +1,26 @@
+#' africa country polygons from GADM
+#'
+#'
+#' @format \code{sf}
+#' @slot data country polygons
+#' @aliases sf_af_gadm0
+#' @name african_country_polygons
+NULL
+
+#' @source \url{https://gadm.org/}
+#' @rdname african_country_polygons
+"sf_af_gadm0"
+
+
+#' african country names and iso 3 letter country codes
+#'
+#'
+#' @format \code{dataframe}
+#' @slot data dataframe
+#' @aliases afcountries
+#' @name african_country_names
+NULL
+
+# @source \url{https://gadm.org/}
+#' @rdname african_country_names
+"afcountries"

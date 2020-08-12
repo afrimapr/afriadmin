a88e6d96650ad18e533bbde71cdb6b896fddc71b
AndySouth <southandy@gmail.com>
2020-08-12 12:42:03 +0100

    animation & vignette outputs probably not all needed

---
+++ vignettes/js/hdx-gadm.js
@@ -0,0 +1,12 @@
+(function($) {
+    $(document).ready(function() {
+	
+	$('#hdx-gadm').scianimator({
+	    'images': ['images/hdx-gadm1.png', 'images/hdx-gadm2.png', 'images/hdx-gadm3.png', 'images/hdx-gadm4.png', 'images/hdx-gadm5.png', 'images/hdx-gadm6.png', 'images/hdx-gadm7.png', 'images/hdx-gadm8.png', 'images/hdx-gadm9.png', 'images/hdx-gadm10.png', 'images/hdx-gadm11.png', 'images/hdx-gadm12.png', 'images/hdx-gadm13.png', 'images/hdx-gadm14.png', 'images/hdx-gadm15.png', 'images/hdx-gadm16.png', 'images/hdx-gadm17.png', 'images/hdx-gadm18.png', 'images/hdx-gadm19.png', 'images/hdx-gadm20.png', 'images/hdx-gadm21.png', 'images/hdx-gadm22.png', 'images/hdx-gadm23.png', 'images/hdx-gadm24.png', 'images/hdx-gadm25.png', 'images/hdx-gadm26.png', 'images/hdx-gadm27.png', 'images/hdx-gadm28.png', 'images/hdx-gadm29.png', 'images/hdx-gadm30.png', 'images/hdx-gadm31.png', 'images/hdx-gadm32.png', 'images/hdx-gadm33.png', 'images/hdx-gadm34.png', 'images/hdx-gadm35.png', 'images/hdx-gadm36.png', 'images/hdx-gadm37.png', 'images/hdx-gadm38.png', 'images/hdx-gadm39.png', 'images/hdx-gadm40.png', 'images/hdx-gadm41.png', 'images/hdx-gadm42.png', 'images/hdx-gadm43.png', 'images/hdx-gadm44.png', 'images/hdx-gadm45.png', 'images/hdx-gadm46.png', 'images/hdx-gadm47.png', 'images/hdx-gadm48.png', 'images/hdx-gadm49.png', 'images/hdx-gadm50.png', 'images/hdx-gadm51.png', 'images/hdx-gadm52.png', 'images/hdx-gadm53.png', 'images/hdx-gadm54.png', 'images/hdx-gadm55.png', 'images/hdx-gadm56.png', 'images/hdx-gadm57.png', 'images/hdx-gadm58.png', 'images/hdx-gadm59.png'],
+	    'width': 480,
+	    'delay': 2000,
+	    'loopMode': 'loop'
+	});
+	$('#hdx-gadm').scianimator('play');
+    });
+})(jQuery);

0be9a3b48289eb3d2cc24a1cf2000777fb2c20f7
AndySouth <southandy@gmail.com>
2020-08-17 17:57:23 +0100

    add geoboundaries

--- R/afriadmin.r
+++ R/afriadmin.r
@@ -14,6 +14,12 @@
 #' @param country a character vector of country names or iso3c character codes.
 #' @param datasource data source, initial default 'gadm'
 #' @param level which admin level to return
+#' @param type for geoboundaries, boundary type defaults to 'simple' One of 'HPSCU', 'HPSCGS', 'SSCGS', 'SSCU', or 'precise' 'simple' 'precise standard' 'simple standard'
+#'  Determines the type of boundary link you receive. More on details
+#' @param version for geoboundaries defaults to the most recent version of geoBoundaries available.
+#'  The geoboundaries version requested, with underscores. For example, 3_0_0 would return data
+#'  from version 3.0.0 of geoBoundaries.
+#' @param quiet for geoboundaries, if TRUE no message while downloading and reading the data. Default to FALSE
 #' @param plot option to display map 'mapview' for interactive, 'sf' for static
 #'
 #'
@@ -25,42 +31,79 @@
 #' @export
 #'
 afriadmin <- function(country,
-                      level = 'max',
-                      datasource = 'gadm',
+                      level = 1, #'max',
+                      datasource = 'geoboundaries',
+                      type = 'simple',
+                      version = NULL,
+                      quiet = FALSE,
                       plot = 'mapview') {
 
-  #GADM_SF_URL  = "https://biogeo.ucdavis.edu/data/gadm3.6/Rsf/gadm36_"
-  #initially store files directly as downloaded from gadm
-
-  path <- system.file(package="afriadmin","/external")
 
   # check and convert country names to iso codes
   iso3c <- country2iso(country)
 
+  # temporarily commented out max level checking
   # find the max admin level available
   # either to check that asked for one is there, or to find max
-  maxlevel <- maxadmin(country=iso3c, datasource=datasource)
+  # maxlevel <- maxadmin(country=iso3c, datasource=datasource)
+  #
+  # if (level=='max')
+  # {
+  #   level <- maxlevel
+  #   message("returning max admin level available for ",iso3c,", ",maxlevel)
+  # }
+  # else if (level > maxlevel)
+  # {
+  #   warning("max admin level available for ",iso3c," is ",maxlevel," you requested ",level)
+  #   #maybe do this to return most detailed available
+  #   level <- maxlevel
+  # }
 
-  if (level=='max')
+
+  #different datasources
+
+  if (datasource == 'geoboundaries')
   {
-    level <- maxlevel
-    message("returning max admin level available for ",iso3c,", ",maxlevel)
+
+    if (!requireNamespace("rgeoboundaries", quietly = TRUE)) {
+      stop("Package \"rgeoboundaries\" needed. You can install it by : remotes::install_gitlab(\"dickoa/rgeoboundaries\")",
+           call. = FALSE)
+    }
+
+    #convert simplified codes to geoboundaries ones
+    if      (type=='simple') type <- 'SSCU'
+    else if (type=='simple standard') type <- 'SSCGS'
+    else if (type=='precise') type <- 'HPSCU'
+    else if (type=='precise standard') type <- 'HPSCGS'
+
+    sf1 <- rgeoboundaries::geoboundaries(iso3c, adm_lvl = paste0('adm',level), type = type, version = version)
+
   }
-  else if (level > maxlevel)
+
+  else if (datasource == 'gadm')
   {
-    warning("max admin level available for ",iso3c," is ",maxlevel," you requested ",level)
-    #maybe do this to return most detailed available
-    level <- maxlevel
+    #when I had data saved in the package
+    #GADM_SF_URL  = "https://biogeo.ucdavis.edu/data/gadm3.6/Rsf/gadm36_"
+    path <- system.file(package="afriadmin","/external")
+
+    filename <- paste0(iso3c,'_adm',level,'.sf.rds')
+
+    sf1 <- readRDS(file.path(path, filename))
   }
 
-  filename <- paste0(iso3c,'_adm',level,'.sf.rds')
-
-  sf1 <- readRDS(file.path(path, filename))
 
   # display map if option chosen
   # helps with debugging, may not be permanent
 
-  if (plot == 'mapview') print(mapview::mapview(sf1, zcol=paste0("NAME_",level), legend=FALSE))
+  if (plot == 'mapview')
+  {
+    zcol <- switch(datasource,
+                   'geoboundaries' = 'shapeName',
+                   'gadm' = paste0("NAME_",level) )
+
+    print(mapview::mapview(sf1, zcol=zcol, legend=FALSE))
+  }
+
   else if (plot == 'sf') plot(sf::st_geometry(sf1))
 
   return(sf1)

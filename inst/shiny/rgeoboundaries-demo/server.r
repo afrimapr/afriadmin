163cd21b81d4a1da9a3e99424489e9acdd1de0bb
AndySouth <southandy@gmail.com>
2021-05-08 12:12:32 +0100

    start min repro example to test errors on shinyapps, copy from afriadmin-compare

---
+++ inst/shiny/rgeoboundaries-demo/server.r
@@ -0,0 +1,130 @@
+# afriadmin/afriadmin-compare/server.r
+# to compare different admin boundaries for the same country (diff. sources and resolutions)
+
+# initially copied from afrihealthsites/healthsites_viewer2
+
+# TODO could add mapshaper to be able to simplify boundaries on the fly
+
+
+#global variables
+
+# to try to allow retaining of map zoom, when selections are changed
+zoom_view <- NULL
+# when country is changed I want whole map to change
+# but with other options (e.g. boundary type) I want to retain zoom
+# perhaps can just reset zoomed view to NULL when country is changed
+
+
+# Define a server for the Shiny app
+function(input, output) {
+
+
+  ######################################
+  # mapview interactive leaflet map plot
+  output$serve_map <- renderLeaflet({
+
+    #avoid problems at start
+    if (length(input$adm_lvl) == 0) return(NULL)
+
+    mapplot <- compareadmin(input$country,
+                            level=input$adm_lvl,
+                            datasource = c('geoboundaries','gadm'),
+                            type = c(input$type, NULL),
+                            plot = 'mapview',
+                            plotshow = FALSE
+                            )
+
+    #creating side-by-side slider view
+    #found that this error was known to others & should be fixed by dev version of mapview
+    #remotes::install_github("r-spatial/mapview")
+    #Warning: Error in value[[3L]]: Couldn't normalize path in `addResourcePath`, with arguments: `prefix` = 'PopupTable-0.0.1'; `directoryPath` = ''
+    #did work from console
+    #mapplot <- mapplot1 | mapplot2
+
+
+    # to retain zoom if only types have been changed
+    if (!is.null(zoom_view))
+    {
+      mapplot@map <- leaflet::fitBounds(mapplot@map, lng1=zoom_view$west, lat1=zoom_view$south, lng2=zoom_view$east, lat2=zoom_view$north)
+    }
+
+
+    #important that this returns the @map bit
+    #otherwise get Error in : $ operator not defined for this S4 class
+    mapplot@map
+
+    })
+
+  ################################################################################
+  # dynamic selectable list of admin levels based on country
+  output$select_lvl <- renderUI({
+
+    # get max for each datasource
+    max_gadm <- afriadmin::maxadmin(input$country, datasource='gadm')
+    max_geob <- afriadmin::maxadmin(input$country, datasource='geoboundaries')
+
+    max_lvl <- max(c(max_gadm,max_geob))
+
+    radioButtons("adm_lvl", label = "admin level",
+                 choices = c(1:max_lvl),
+                 inline = TRUE, #horizontal
+                 selected = 1)
+  })
+
+
+  #########################################################################
+  # trying to detect map zoom as a start to keeping it when options changed
+  observeEvent(input$serve_map_bounds, {
+
+    #print(input$serve_healthsites_map_bounds)
+
+    #save to a global object so can reset to it
+    zoom_view <<- input$serve_map_bounds
+  })
+
+  ####################################################################
+  # perhaps can just reset zoomed view to NULL when country is changed
+  # hurrah! this works, is it a hack ?
+  observe({
+    input$country
+    zoom_view <<- NULL
+  })
+
+
+  ###################################
+  # to update map without resetting everything use leafletProxy
+  # see https://rstudio.github.io/leaflet/shiny.html
+  # Incremental changes to the map should be performed in
+  # an observer. Each independent set of things that can change
+  # should be managed in its own observer.
+  # BUT I don't quite know how to use with a mapview map ...
+  # observe({
+  #   #pal <- colorpal()
+  #   # leafletProxy("map", data = filteredData()) %>%
+  #   #   clearShapes() %>%
+  #   #   addCircles(radius = ~10^mag/10, weight = 1, color = "#777777",
+  #   #              fillColor = ~pal(mag), fillOpacity = 0.7, popup = ~paste(mag)
+  #   #  )
+  # })
+
+
+
+
+  ###########################
+  # table of admin unit names from the 2 sources
+  output$table_names <- DT::renderDataTable({
+
+    #want to avoid downloading the data again,
+    #but hopefully it will have been cached locally so won't download again
+
+    #beware this just uses single type so far
+    dfnames <- compareadmin(input$country, level=input$adm_lvl, type=input$type, plot='namestable')
+
+
+    DT::datatable(dfnames, options = list(pageLength = 50))
+  })
+
+
+
+
+}
---
+++ inst/shiny/rgeoboundaries-demo/ui.r
@@ -0,0 +1,102 @@
+# afriadmin/afriadmin-compare/ui.r
+# to compare different admin boundaries for the same country (diff. sources and resolutions)
+
+# initially copied from afrihealthsites/healthsites_viewer2
+
+cran_packages <- c("shiny","leaflet","remotes")
+
+lapply(cran_packages, function(x) if(!require(x,character.only = TRUE)) install.packages(x))
+
+library(shiny)
+library(leaflet)
+library(remotes)
+
+if(!require(afriadmin)){
+  remotes::install_github("afrimapr/afriadmin")
+}
+
+#IMPORTANT TO INSTALL rgeoboundaries from github rather than gitlab to get it to work on shinyapps
+#remotes::install_github("wmgeolab/rgeoboundaries")
+
+library(afriadmin)
+library(rgeoboundaries) #think maybe this shouldn't be needed but seems to cause fail on shinyapps
+library(mapview) #otherwise | operator doesn't work
+
+#
+data(afcountries)
+
+fluidPage(
+
+  #headerPanel('afrimapr admin boundaries comparison tool'),
+  headerPanel(p( a("afrimapr", href="http://www.afrimapr.org", target="_blank"),
+                 'admin boundaries comparison app')),
+
+  p("compare", a("geoBoundaries", href="https://www.geoboundaries.org/", target="_blank"),
+    "&",       a("GADM", href="https://www.gadm.org/", target="_blank") ,
+    "- may take a few seconds to download selected data"),
+
+  sidebarLayout(
+
+  sidebarPanel( width=3,
+
+    # p("data from",
+    #               a("healthsites.io", href="https://www.healthsites.io", target="_blank"),
+    #               " & ",
+    #               a("KEMRI Wellcome", href="https://www.nature.com/articles/s41597-019-0142-2", target="_blank"),
+    #               " / ",
+    #               a("WHO", href="https://www.who.int/malaria/areas/surveillance/public-sector-health-facilities-ss-africa/en/", target="_blank")),
+
+
+    #selectInput('country', 'Country', afcountries$name, size=10, selectize=FALSE, multiple=TRUE, selected="Angola"),
+    #miss out Western Sahara because no healthsites or WHO
+    selectInput('country', 'Country', choices = sort(afcountries$name[!afcountries$name=="Western Sahara"]),
+                size=7, selectize=FALSE, multiple=TRUE, selected="Angola"),
+
+    #admin level
+    uiOutput("select_lvl"),
+    # selectInput("adm_lvl", label = "admin level",
+    #             choices = c(1:4),
+    #             selected = 1),
+
+
+    selectInput("type", label = "geoboundaries type",
+                choices = c("simple (sscu)"="sscu", "precise (hpscu)"="hpscu", "simple standard (sscgs)"="sscgs", "precise standard (hpscgs)"="hpscgs"),
+                selected = 1),
+
+    # dynamic who category selection
+    #uiOutput("select_who_cat"),
+
+    p("active development Sep 2020, v0.1 ",
+      "Open source ", a("R code", href="https://github.com/afrimapr/afriadmin", target="_blank")),
+
+    #p("Contact : ", a("@southmapr", href="https://twitter.com/southmapr", target="_blank")),
+    #p("Open source ", a("R code", href="https://github.com/afrimapr/afriadmin", target="_blank")),
+
+    p("by ", a("afrimapr", href="http://www.afrimapr.org", target="_blank"),
+      ": creating R building-blocks to ease use of open health data in Africa"),
+
+    p("Input and suggestions ", a("welcome", href="https://github.com/afrimapr/suggestions_and_requests", target="_blank")),
+    #  "Contact : ", a("@southmapr", href="https://twitter.com/southmapr", target="_blank")),
+
+    p(tags$small("Disclaimer : Data used by afrimapr are sourced from published open data sets. We provide no guarantee of accuracy.")),
+
+  ),
+
+  mainPanel(
+
+    #when just had the map
+    #leafletOutput("serve_map", height=1000)
+
+    #tabs
+    tabsetPanel(type = "tabs",
+                tabPanel("map", leafletOutput("serve_map", height=800)),
+                tabPanel("names", DT::dataTableOutput("table_names")))
+                #tabPanel("facility types", plotOutput("plot_fac_types", height=600)),
+                #tabPanel("healthsites data", DT::dataTableOutput("table_raw_hs")),
+
+    )
+  )
+)
+
+
+
